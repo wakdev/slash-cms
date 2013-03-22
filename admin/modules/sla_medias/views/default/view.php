82dea6f624af26e9a97a2f13655ebb9a47577e88
wakdev <julien@wakdev.com>
2013-03-22 18:38:11 +0100

    Update sla_medias module

--- admin/modules/sla_medias/views/default/view.php
+++ admin/modules/sla_medias/views/default/view.php
@@ -34,6 +34,7 @@ class sla_medias_view extends slaView implements iView {
 	public function header () {
 		
 		sl_interface::script("../core/plugins/jquery_plugins/preload/js/preloadCssImages.js");
+		sl_interface::script("modules/sla_medias/views/default/js/sla_medias.js");
 		sl_interface::stylesheet("modules/sla_medias/views/default/css/assets.css");
 	}
 	
@@ -52,22 +53,21 @@ class sla_medias_view extends slaView implements iView {
 		
 		echo "<table align='right'><tr>";
 		
-		echo "<td align='center' width='30%'><a href='javascript:void(0);' class='sla_media_myfiles_button'
-			onClick=\"$('#sla_medias_frame').attr('src', '../core/plugins/kcfinder/browse.php?type=files&lang=fr');\"></a>".$this->slash->trad_word("MEDIAS_MYFILES")."</td>";
+		echo "<td align='center' width='30%'><a href='javascript:void(0);' id='sla_medias_files' class='sla_medias_myfiles_button sla_medias_current'
+			onClick=\"javascript:setFinder('files','".$_SESSION["user_language"]."');\"></a>".$this->slash->trad_word("MEDIAS_MYFILES")."</td>";
 		
-		echo "<td align='center' width='30%'><a href='javascript:void(0);' class='sla_media_myimages_button'
-			onClick=\"$('#sla_medias_frame').attr('src', '../core/plugins/kcfinder/browse.php?type=images&lang=fr');\"></a>".$this->slash->trad_word("MEDIAS_MYIMAGES")."</td>";
+		echo "<td align='center' width='30%'><a href='javascript:void(0);' id='sla_medias_images' class='sla_medias_myimages_button'
+			onClick=\"javascript:setFinder('images','".$_SESSION["user_language"]."');\"></a>".$this->slash->trad_word("MEDIAS_MYIMAGES")."</td>";
 		
-		echo "<td align='center' width='30%'><a href='javascript:void(0);' class='sla_media_mymedias_button'
-			onClick=\"$('#sla_medias_frame').attr('src', '../core/plugins/kcfinder/browse.php?type=media&lang=fr');\"></a>".$this->slash->trad_word("MEDIAS_MYMEDIAS")."</td>";
+		echo "<td align='center' width='30%'><a href='javascript:void(0);' id='sla_medias_media' class='sla_medias_mymedia_button'
+			onClick=\"javascript:setFinder('media','".$_SESSION["user_language"]."');\"></a>".$this->slash->trad_word("MEDIAS_MYMEDIAS")."</td>";
 		echo "</tr></table>";
 				
 		echo "</td></tr></table>";
 		
-		echo "<iframe id='sla_medias_frame' name='sla_medias_frame' src='../core/plugins/kcfinder/browse.php?type=files&lang=fr' width='99%' height='500'></iframe>";
-					
-	
 		
+		echo "<iframe id='sla_medias_frame' name='sla_medias_frame' src='../core/plugins/kcfinder/browse.php?type=files&lang=".$_SESSION["user_language"]."' width='100%' height='450'></iframe>";
+					
 		
 	}
 	
